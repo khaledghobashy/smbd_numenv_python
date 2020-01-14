c1672b858d630c8dce53ae7399934cd16d6a7dbc
Khaled Ghobashy <khaled.ghobashy@live.com>
2020-01-14 11:25:54 +0200

    changed __all__

--- uraeus/nmbd/python/__init__.py
+++ uraeus/nmbd/python/__init__.py
@@ -2,6 +2,7 @@
 #__import__('pkg_resources').declare_namespace(__name__)
 
 from .numerics.core.systems import multibody_system, simulation, configuration
-from .codegen import projects as numenv
+from .codegen.projects import standalone_project, templatebased_project
 
-__all__ = ['multibody_system', 'simulation', 'configuration', 'numenv']
+__all__ = ['multibody_system', 'simulation', 'configuration', 
+           'standalone_project', 'standalone_project']
