04bd7f1f689d0ec414562b881bc008b18b8e601a
Khaled Ghobashy <khaled.ghobashy@live.com>
2020-01-22 14:53:56 +0200

    added 'assembly' class that construct numerical assembly from a JSON file

--- uraeus/nmbd/python/numerics/core/systems.py
+++ uraeus/nmbd/python/numerics/core/systems.py
@@ -6,16 +6,21 @@ Created on Wed Mar 27 08:49:16 2019
 """
 # Standard library imports
 import os
+import json
 import pickle
 import itertools
+import functools
+import importlib.util
+from collections import namedtuple
 
 # 3rd party library imports
 import numpy as np
+from numpy.linalg import multi_dot
 
 # Local applicataion imports
 from .solvers import kds_solver, dds_solver
 from .utilities.decoders import JSON_Decoder
-
+from .math_funcs import G
 
 ###############################################################################
 
@@ -28,9 +33,11 @@ def load_pickled_data(file):
 ###############################################################################
 
 class multibody_system(object):
-    
     def __init__(self, system):
-        self.topology = system.topology()
+        try:
+            self.topology = system.topology()
+        except AttributeError:
+            self.topology = system
         try:
             self.Subsystems = system.subsystems
         except AttributeError:
@@ -40,7 +47,6 @@ class multibody_system(object):
 ###############################################################################
 
 class simulation(object):
-    
     def __init__(self, name, model, typ='kds'):
         self.name = name
         self.assembly = model.topology
@@ -72,7 +78,6 @@ class simulation(object):
 ###############################################################################
 
 class configuration(object):
-    
     def __init__(self, name):
         self.name = name
 
@@ -103,4 +108,206 @@ class configuration(object):
         for key in _attributes:
             value = getattr(self, key)
             setattr(self, key, value)
+
+###############################################################################
+###############################################################################
+
+# https://stackoverflow.com/questions/31174295/getattr-and-setattr-on-nested-subobjects-chained-properties
+
+def rsetattr(obj, attr, val):
+    pre, _, post = attr.rpartition('.')
+    return setattr(rgetattr(obj, pre) if pre else obj, post, val)
+
+def rgetattr(obj, attr, *args):
+    def _getattr(obj, attr):
+        return getattr(obj, attr, *args)
+    return functools.reduce(_getattr, [obj] + attr.split('.'))
+
+
+
+def subsystems_creator(mapping, templates_dir):
+    container = namedtuple('Subsystems', mapping.keys())
+    data = {}
+    for subsystem, template in mapping.items():
+        spec = importlib.util.spec_from_file_location(template, f"{templates_dir}/{template}.py")
+        foo = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(foo)
+        data[subsystem] = foo.topology(subsystem)
+    return container(**data)
+
+class assembly(object):
+    def __init__(self, json_file, templates_dir):
+        assembly_data = self.construct_from_json(json_file)
+
+        self.subsystems = subsystems_creator(assembly_data['subsystems'], templates_dir)
+        self.interface_map = assembly_data['interface_map']
+        self.indicies_map  = assembly_data['nodes_indicies']
+        self.mapped_vir_coordinates  = assembly_data['mapped_vir_coordinates']
+        self.mapped_vir_velocities  = assembly_data['mapped_vir_velocities']
+        self.mapped_vir_accelerations  = assembly_data['mapped_vir_accelerations']
+
+        self.nrows = sum([sub.nrows for sub in self.subsystems]) + 2
+        self.ncols = sum([sub.ncols for sub in self.subsystems]) + 2
+        self.n  = sum([sub.n for sub in self.subsystems]) + 7
+        self.nc = sum([sub.nc for sub in self.subsystems]) + 7
+
+        self.R_ground  = np.array([[0], [0], [0]], dtype=np.float64)
+        self.P_ground  = np.array([[1], [0], [0], [0]], dtype=np.float64)
+        self.Pg_ground = np.array([[1], [0], [0], [0]], dtype=np.float64)
+
+        self.m_ground = np.eye(3, dtype=np.float64)
+        self.Jbar_ground = np.eye(3, dtype=np.float64)
+
+        self.gr_rows = np.array([0, 1])
+        self.gr_jac_rows = np.array([0, 0, 1, 1])
+        self.gr_jac_cols = np.array([0, 1, 0, 1])
+        self._t = 0
+
+    @property
+    def t(self):
+        return self._t
+    @t.setter
+    def t(self,t):
+        self._t = t
+        for sub in self.subsystems:
+            sub.t = t
+
+    @staticmethod
+    def construct_from_json(json_file):
+        with open(json_file, 'r') as f:
+            json_text = f.read()
+        data_dict = json.loads(json_text)
+        return data_dict
+
+    def set_initial_states(self):
+        for sub in self.subsystems:
+            sub.set_initial_states()
+        coordinates = [sub.q0 for sub in self.subsystems if len(sub.q0)!=0]
+        self.q0 = np.concatenate([self.R_ground, self.P_ground, *coordinates])
+
+    def initialize(self):
+        self.t = 0
+        self.assemble()
+        self.set_initial_states()
+        self.eval_constants()
+
+    def assemble(self):
+        offset = 2
+        for sub in self.subsystems:
+            sub.assemble(self.indicies_map, self.interface_map, offset)
+            offset += sub.nrows
+
+        self.rows = np.concatenate([s.rows for s in self.subsystems])
+        self.jac_rows = np.concatenate([s.jac_rows for s in self.subsystems])
+        self.jac_cols = np.concatenate([s.jac_cols for s in self.subsystems])
+
+        self.rows = np.concatenate([self.gr_rows,self.rows])
+        self.jac_rows = np.concatenate([self.gr_jac_rows,self.jac_rows])
+        self.jac_cols = np.concatenate([self.gr_jac_cols,self.jac_cols])
+
+        self.reactions_indicies = sum([sub.reactions_indicies for sub in self.subsystems],[])
+
+    def _map_coordinates(self, mapping, config=False):
+        for virtual, actual in mapping.items():
+            if config:
+                virtual = '%s.config.%s'%tuple(virtual.split('.'))
+                try:
+                    actual  = '%s.config.%s'%tuple(actual.split('.'))
+                except TypeError:
+                    pass
+            try:
+                rsetattr(self.subsystems, virtual, rgetattr(self.subsystems, actual))
+            except AttributeError:
+                rsetattr(self.subsystems, virtual, rgetattr(self, actual))
+        
+    def eval_constants(self):
+        self._map_coordinates(self.mapped_vir_coordinates, config=True)
+        for sub in self.subsystems:
+            sub.eval_constants()
+
+    def set_gen_coordinates(self,q):
+        self.R_ground = q[0:3,0:1]
+        self.P_ground = q[3:7,0:1]
+        offset = 7
+        for sub in self.subsystems:
+            qs = q[offset:sub.n+offset]
+            sub.set_gen_coordinates(qs)
+            offset += sub.n
+        self._map_coordinates(self.mapped_vir_coordinates)
+
+    def set_gen_velocities(self,qd):
+        self.Rd_ground = qd[0:3,0:1]
+        self.Pd_ground = qd[3:7,0:1]
+        offset = 7
+        for sub in self.subsystems:
+            qs = qd[offset:sub.n+offset]
+            sub.set_gen_velocities(qs)
+            offset += sub.n
+        self._map_coordinates(self.mapped_vir_velocities)
+
+    def set_gen_accelerations(self,qdd):
+        self.Rdd_ground = qdd[0:3,0:1]
+        self.Pdd_ground = qdd[3:7,0:1]
+        offset = 7
+        for sub in self.subsystems:
+            qs = qdd[offset:sub.n+offset]
+            sub.set_gen_accelerations(qs)
+            offset += sub.n
+        self._map_coordinates(self.mapped_vir_accelerations)
+
+    def set_lagrange_multipliers(self,Lambda):
+        offset = 7
+        for sub in self.subsystems:
+            l = Lambda[offset:sub.nc+offset]
+            sub.set_lagrange_multipliers(l)
+            offset += sub.nc
+
+    def eval_pos_eq(self):
+        pos_ground_eq_blocks = [self.R_ground,(-1*self.Pg_ground + self.P_ground)]
+        for sub in self.subsystems:
+            sub.eval_pos_eq()
+        eq_blocks = (s.pos_eq_blocks for s in self.subsystems)
+        self.pos_eq_blocks = (*pos_ground_eq_blocks, *itertools.chain(*eq_blocks))
+    
+    def eval_vel_eq(self):
+        vel_ground_eq_blocks = [np.zeros((3,1),dtype=np.float64),np.zeros((4,1),dtype=np.float64)]
+        for sub in self.subsystems:
+            sub.eval_vel_eq()
+        eq_blocks = (s.vel_eq_blocks for s in self.subsystems)
+        self.vel_eq_blocks = (*vel_ground_eq_blocks, *itertools.chain(*eq_blocks))
+
+    def eval_acc_eq(self):
+        acc_ground_eq_blocks = [np.zeros((3,1),dtype=np.float64),np.zeros((4,1),dtype=np.float64)]
+        for sub in self.subsystems:
+            sub.eval_acc_eq()
+        eq_blocks = (s.acc_eq_blocks for s in self.subsystems)
+        self.acc_eq_blocks = (*acc_ground_eq_blocks, *itertools.chain(*eq_blocks))
+
+    def eval_jac_eq(self):
+        jac_ground_eq_blocks = [np.eye(3, dtype=np.float64),np.zeros((3,4),dtype=np.float64),np.zeros((4,3),dtype=np.float64),np.eye(4, dtype=np.float64)]
+        for sub in self.subsystems:
+            sub.eval_jac_eq()
+        eq_blocks = (s.jac_eq_blocks for s in self.subsystems)
+        self.jac_eq_blocks = (*jac_ground_eq_blocks, *itertools.chain(*eq_blocks))
+    
+    def eval_mass_eq(self):
+        mass_ground_eq_blocks = [self.m_ground*np.eye(3, dtype=np.float64),4*multi_dot([G(self.P_ground).T,self.Jbar_ground,G(self.P_ground)])]
+        for sub in self.subsystems:
+            sub.eval_mass_eq()
+        eq_blocks = (s.mass_eq_blocks for s in self.subsystems)
+        self.mass_eq_blocks = (*mass_ground_eq_blocks, *itertools.chain(*eq_blocks))
+
+    def eval_frc_eq(self):
+        frc_ground_eq_blocks = [np.zeros((3,1),dtype=np.float64),np.zeros((4,1),dtype=np.float64)]
+        for sub in self.subsystems:
+            sub.eval_frc_eq()
+        eq_blocks = (s.frc_eq_blocks for s in self.subsystems)
+        self.frc_eq_blocks = (*frc_ground_eq_blocks, *itertools.chain(*eq_blocks))
+
+    def eval_reactions_eq(self):
+        self.reactions = {}
+        for sub in self.subsystems:
+            sub.eval_reactions_eq()
+            for k,v in sub.reactions.items():
+                self.reactions['%s%s'%(sub.prefix,k)] = v
 
