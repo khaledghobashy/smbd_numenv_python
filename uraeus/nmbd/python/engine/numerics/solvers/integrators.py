8a5179b2094f541079272094a5835f38f4e9ce59
Khaled Ghobashy <khaled.ghobashy@live.com>
2020-03-03 16:01:18 +0200

    added custome RK integrators

--- uraeus/nmbd/python/engine/numerics/solvers/integrators.py
+++ uraeus/nmbd/python/engine/numerics/solvers/integrators.py
@@ -5,4 +5,113 @@ Created on Mon Mar  2 20:10:35 2020
 @author: khaled.ghobashy
 """
 
+import numpy as np
 
+class integrator(object):
+
+    def __init__(self, SSODE, y0, t0, t_end, h, **kwargs):
+
+        self.SSODE = SSODE
+        self.h = h
+        self.t = t0
+
+    def step(self):
+        raise NotImplementedError
+
+class RKMethods(integrator):
+
+    n_stages = NotImplemented
+
+    A = NotImplemented
+    B = NotImplemented
+    C = NotImplemented
+
+    def __init__(self, SSODE, y0, t0, t_end, h, **kwargs):
+        self.K = np.empty((self.n_stages + 1, y0.shape[0]))
+        super().__init__(SSODE, y0, t0, t_end, h, **kwargs)
+
+    def step(self, state_vector, i, fi):
+        
+        h = self.h
+        t = self.t
+        func = self.SSODE
+
+        A = self.A
+        B = self.B
+        C = self.C
+        K = self.K
+
+        K[0] = fi.flat
+
+        for s, (a, c) in enumerate(zip(A[1:], C[1:]), start=1):
+            dy   = h * (K[:s].T @ a[:s][:, None])
+            K[s] = func(state_vector + dy, t + c * h, i).flat
+        
+        yn = state_vector +  (h * (K[:-1].T @ B[:, None]))
+
+        #print(yn, yn.shape)
+        #f_new = func(yn, t + h, i)
+        #K[-1] = f_new.flat
+
+        self.t = t + h
+        self.y = yn
+    
+
+class Explicit_RK4(RKMethods):
+
+    n_stages = 4
+
+    A = np.array([[0, 0, 0,   0],
+                  [1/2, 0, 0, 0],
+                  [0, 1/2, 0, 0],
+                  [0,  0, 1,  0]])
+    
+    B = np.array([1/6, 1/3, 1/3, 1/6])
+    C = np.array([0, 1/2, 1/2, 0])
+
+
+class Explicit_RK2(RKMethods):
+
+    n_stages = 2
+
+    A = np.array([[0, 0],
+                  [2/3, 0]])
+    
+    B = np.array([1/4, 3/4])
+    C = np.array([0, 2/3])
+
+
+class Explicit_RK23(RKMethods):
+
+    n_stages = 3
+
+    A = np.array([[0, 0, 0],
+                  [1/2, 0, 0],
+                  [0, 3/4, 0]])
+    B = np.array([2/9, 1/3, 4/9])
+    C = np.array([0, 1/2, 3/4])
+
+
+class Explicit_RK45(RKMethods):
+
+    n_stages = 6
+
+    A = np.array([
+        [0, 0, 0, 0, 0],
+        [1/5, 0, 0, 0, 0],
+        [3/40, 9/40, 0, 0, 0],
+        [44/45, -56/15, 32/9, 0, 0],
+        [19372/6561, -25360/2187, 64448/6561, -212/729, 0],
+        [9017/3168, -355/33, 46732/5247, 49/176, -5103/18656]
+    ])
+
+    B = np.array([35/384, 0, 500/1113, 125/192, -2187/6784, 11/84])
+    C = np.array([0, 1/5, 3/10, 4/5, 8/9, 1])
+
+
+from scipy.optimize import fsolve
+
+class Implicit_Trapezoidal(integrator):
+
+
+    pass
--- uraeus/nmbd/python/engine/numerics/solvers/solvers.py
+++ uraeus/nmbd/python/engine/numerics/solvers/solvers.py
@@ -19,6 +19,9 @@ import numba
 
 # Local imports.
 from ..math_funcs.numba_funcs import matrix_assembler
+from .integrators import (Explicit_RK4, Explicit_RK23, 
+                          Explicit_RK45, Explicit_RK2, 
+                          Implicit_Trapezoidal)
 
 
 ###############################################################################
@@ -209,7 +212,7 @@ class abstract_solver(object):
         delta_q = solve(A, -b)
         
         itr=0
-        while np.linalg.norm(delta_q)>1e-5:
+        while np.linalg.norm(delta_q)>1e-4:
 #            print(np.linalg.norm(delta_q))
             guess = guess + delta_q
             
@@ -299,7 +302,7 @@ class kds_solver(abstract_solver):
 ###############################################################################
 ###############################################################################
 
-class dds_solver(abstract_solver):
+class dds_solver1(abstract_solver):
     
     def __init__(self, model):
         super().__init__(model)
@@ -507,4 +510,212 @@ class dds_solver(abstract_solver):
         return self._lgr_history[i]
         
 
+#################################################################################################
 
+class dds_solver(abstract_solver):
+    
+    def __init__(self, model):
+        super().__init__(model)
+        if self.model.n == self.model.nc:
+            raise ValueError('Model is fully constrained.')
+        
+    
+    def solve(self, run_id):
+        t0 = time.perf_counter()
+        
+        time_array = self.time_array
+        dt = self.step_size
+        bar_length = len(time_array)-1
+        
+        print('\nStarting System Dynamic Analysis:')
+        
+        self._extract_independent_coordinates()
+        print('Estimated DOF : %s'%(len(self.independent_cord),))
+        print('Estimated Independent Coordinates : %s'%(self.independent_cord,))
+        
+        pos_t0 = self._pos_history[0]
+        vel_t0 = self._vel_history[0]
+        
+        self._newton_raphson(pos_t0)
+
+        M, J, Qt, Qd = self._eval_augmented_matricies(pos_t0, vel_t0)
+        acc_t0, lamda_t0 = self._solve_augmented_system(M, J, Qt, Qd)        
+        self._acc_history[0] = acc_t0
+        self._lgr_history[0] = lamda_t0
+
+        q_v  = self.get_indpenednt_q(pos_t0)
+        qd_v = self.get_indpenednt_q(vel_t0)
+        state_vector = np.concatenate([q_v, qd_v])
+
+        self.integrator = Explicit_RK23(self.SSODE, state_vector, 0, time_array[-1], dt)
+        
+        print('\nRunning System Dynamic Analysis:')
+        i = 0
+        while i != bar_length:
+            progress_bar(bar_length, i, t0)
+            t = time_array[i+1]
+            self._extract_independent_coordinates(self._jac[:-self.dof])
+            self._set_time(t)
+            self._solve_time_step(t, i, dt)
+            i += 1            
+        print('\n')
+        self._creat_results_dataframes()
+
+    def _solve_time_step(self, t, i, dt):
+        
+        q   = self._pos_history[i]
+        qd  = self._vel_history[i]
+        qdd = self._acc_history[i]
+        
+        q_v   = self.get_indpenednt_q(q)
+        qd_v  = self.get_indpenednt_q(qd)
+        qdd_v = self.get_indpenednt_q(qdd)
+
+        state_vector = np.concatenate([q_v, qd_v])
+        current_derivative = np.concatenate([qd_v, qdd_v])
+        
+        self.integrator.step(state_vector, i, current_derivative)
+        soln = self.integrator.y
+
+        y1 = soln[:self.dof]
+        y2 = soln[self.dof:]
+
+        guess = q + (qd * dt) + (0.5 * qdd * (dt**2))
+
+        for c in range(self.dof): 
+            guess[np.argmax(self.independent_cols[:, c]), 0] = y1[c,0]
+        
+        self._newton_raphson(guess)
+        A  = self._jac
+        qi = self._pos
+        
+        vel_rhs = self._eval_vel_eq(y2)
+        vi = solve(A, -vel_rhs)
+        
+        self._set_gen_coordinates(qi)
+        self._set_gen_velocities(vi)
+        
+        J  = A[:-self.dof]
+        M  = self._eval_mass_eq()
+        Qt = self._eval_frc_eq()
+        Qd = self._eval_acc_eq()
+        acc_ti, lamda_ti = self._solve_augmented_system(M, J, Qt, Qd)
+        
+        self._pos_history[i+1] = qi
+        self._vel_history[i+1] = vi
+        self._acc_history[i+1] = acc_ti
+        self._lgr_history[i+1] = lamda_ti
+        
+#    @profile
+    def SSODE(self, state_vector, t, i):
+          
+        self._set_time(t)
+        
+        y1 = state_vector[:self.dof]
+        y2 = state_vector[self.dof:]
+        
+        dt = self.step_size
+        
+        guess = self._pos_history[i] \
+              + self._vel_history[i]*dt \
+              + 0.5*self._acc_history[i]*(dt**2)
+
+        for c in range(self.dof): 
+            guess[np.argmax(self.independent_cols[:, c]), 0] = y1[c,0]
+                        
+        self._newton_raphson(guess)
+        self._set_gen_coordinates(self._pos)
+                
+        vel_rhs = self._eval_vel_eq(y2)
+        vi = solve(self._jac, -vel_rhs)
+        self._set_gen_velocities(vi)
+
+        J  = self._jac[:-self.dof]
+        M  = self._eval_mass_eq()
+        Qt = self._eval_frc_eq()
+        Qd = self._eval_acc_eq()
+        acc_ti, lamda_ti = self._solve_augmented_system(M, J, Qt, Qd)
+        
+        y3 = self.get_indpenednt_q(acc_ti)
+        
+        rhs_vector = np.concatenate([y2, y3])
+        
+        return rhs_vector
+        
+
+    def _extract_independent_coordinates(self, jacobian=None):
+        A = super()._eval_jac_eq() if jacobian is None else jacobian
+        rows, cols = A.shape
+        permutaion_mat = sc.linalg.lu(A.T)[0]
+        independent_cols = permutaion_mat[:, rows:]
+        self.dof = dof = independent_cols.shape[1]
+        independent_cord = [self._coordinates_indicies[np.argmax(independent_cols[:,i])] for i in range(dof) ]
+        self.permutaion_mat  = permutaion_mat.T
+        self.independent_cols = independent_cols
+        self.independent_cord = independent_cord
+    
+    
+    def _eval_augmented_matricies(self, q , qd):
+        self._set_gen_coordinates(q)
+        self._set_gen_velocities(qd)
+        J  = super()._eval_jac_eq()
+        M  = self._eval_mass_eq()
+        Qt = self._eval_frc_eq()
+        Qd = self._eval_acc_eq()
+        return M, J, Qt, Qd
+    
+        
+    def _solve_augmented_system(self, M, J, Qt, Qd):
+        
+        z = np.zeros((self.model.nc, self.model.nc))
+
+        u = np.concatenate([M, J.T], axis=1)
+        l = np.concatenate([J, z], axis=1)
+        
+        A = np.concatenate([u, l], axis=0)
+#        A = sc.sparse.coo_matrix(A)
+        
+        b = np.concatenate([Qt, -Qd])
+        x = solve(A, b)
+        n = self.model.n
+        accelerations = x[:n]
+        lamda = x[n:]
+        return accelerations, lamda
+    
+    def _eval_pos_eq(self):
+        A = super()._eval_pos_eq()
+        Z = np.zeros((self.dof, 1))
+        A = np.concatenate([A, Z])
+        return A
+
+    def _eval_vel_eq(self,ind_vel_i):
+        A = super()._eval_vel_eq()
+        V = np.array(ind_vel_i).reshape((self.dof, 1))
+        A = np.concatenate([A, -V])
+        return A
+    
+    def _eval_jac_eq(self):
+        A = np.concatenate([super()._eval_jac_eq(), self.independent_cols.T])
+        return A
+    
+            
+    def get_indpenednt_q(self, q):
+        dof = self.dof
+        P = self.permutaion_mat
+        qp = P@q
+        qv = qp[-dof:, :]
+        return qv
+    
+        
+    
+    @staticmethod
+    def _forward_euler(func, state_vector, t, h, i):
+        f1 = func(state_vector, t, i)
+        yn = state_vector + h*f1
+        return yn
+    
+    
+    def _eval_lagrange_multipliers(self, i):
+        self._set_gen_coordinates(self._pos_history[i])
+        return self._lgr_history[i]
+        
