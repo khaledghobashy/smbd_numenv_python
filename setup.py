003dadab2f40cd7eae092345aef7e4eab65f93b5
Khaled Ghobashy <khaled.ghobashy@live.com>
2020-11-26 10:24:40 +0200

    fall back to prior format

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
 import setuptools
-import numpy as np
-from distutils.extension import Extension
-from Cython.Build import cythonize
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 short_description = 'Numerical simulation environment of constrained multi-body systems in python.'
-
-ext_modules = [Extension("uraeus.nmbd.python.engine.numerics.math_funcs._cython_definitions.matrix_funcs", 
-                         [r"uraeus\nmbd\python\engine\numerics\math_funcs\_cython_definitions\matrix_funcs.pyx"])]
-
 
 
 setuptools.setup(
@@ -22,7 +15,7 @@ setuptools.setup(
     description = short_description,
     long_description = long_description,
     long_description_content_type = "text/markdown",
-    url = "https://github.com/khaledghobashy/uraeus-nmbd-python",
+    url = "https://github.com/khaledghobashy/uraeus_nmbd_python",
     packages = ['uraeus.nmbd'] + setuptools.find_packages(exclude=("tests",)),
     classifiers = [
         "Programming Language :: Python :: 3",
@@ -35,8 +28,6 @@ setuptools.setup(
         "Topic :: Scientific/Engineering :: Physics",
         "Topic :: Scientific/Engineering :: Mathematics"
     ],
-    ext_modules = cythonize(ext_modules, compiler_directives = {'language_level' : "3"}),
-    include_dirs = [np.get_include()],
     python_requires = '>=3.6',
     install_requires=[
           'uraeus.smbd>=0.0.1.dev3',
